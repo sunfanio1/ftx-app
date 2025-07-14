ff24313e928c0a13b2b25ae73a7ac8ce2bbcd230
Игорь Иженбин <izhenbin_ia@lemz-t.ru>
2025-07-14 23:33:04 +0700

    add some monitoring

--- services/orders/src/api_app.py
+++ services/orders/src/api_app.py
@@ -1,5 +1,10 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
+
+import sentry_sdk
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.routes.v1 import orders
 
@@ -8,7 +13,21 @@ def create_app() -> FastAPI:
     app.include_router(orders.router, prefix="/api")
     return app
 
+app = create_app()
+
+Instrumentator().instrument(app).expose(app)
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    traces_sample_rate=1.0,  # Включение трейсинга (0.0–1.0)
+    environment="development",
+    release="1.0.0",
+)
+
+
+
+
 if __name__ == "__main__":
-    uvicorn.run(create_app(),
+    uvicorn.run(app,
         host="0.0.0.0",
         port=8000)
--- services/orders/src/routes/v1/orders.py
+++ services/orders/src/routes/v1/orders.py
@@ -1,15 +1,34 @@
 from fastapi import APIRouter
-
+from confluent_kafka import Consumer, Producer, KafkaException
 
 router = APIRouter(prefix="/v1", tags=["Orders"])
+
+config_producer = {
+    'bootstrap.servers': 'kafka:9092'
+}
+
+config_consumer = {
+    'bootstrap.servers': 'kafka:9092',  # Список серверов Kafka
+    'group.id': 'mygroup',                  # Идентификатор группы потребителей
+    'auto.offset.reset': 'earliest'         # Начальная точка чтения ('earliest' или 'latest')
+}
 
 @router.get("/orders/{order_id}")
 async def get_user_order():
     return {"message": "Hello World"}
 
+@router.get("/orders")
+async def get_orders():
+
+    return {"kafka_data": "message"}
+
+
 @router.post("/orders")
-async def create_order():
-    return {"message": "Hello World"}
+async def create_order(data: str):
+    producer = Producer(config_producer)
+    producer.produce('test_topic', data)
+    producer.flush()
+    return {"message": data}
 
 @router.patch("/orders/{order_id}")
 async def change_order():
